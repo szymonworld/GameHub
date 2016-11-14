3f031460baf0dcbef1a533fb12d9d2e46e4333c0
Szymon Holisz <szymonworld@gmail.com>
2016-11-14 23:17:07 +0100

    GameHub
    v0.1

---
+++ GameHub/GameHub/MainActivity.cs
@@ -0,0 +1,67 @@
+﻿using Android.App;
+using Android.Views;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Support.V4.Widget;
+using Android.Widget;
+using SupportToolbar = Android.Support.V7.Widget.Toolbar;
+using Android.Support.Design.Widget;
+
+
+namespace GameHub
+{
+    [Activity(Label = "GameHub", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/MyTheme")]
+
+
+
+    public class MainActivity : AppCompatActivity
+    {
+
+        private DrawerLayout mDrawerLayout;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+            var toolbar = FindViewById<SupportToolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+            SupportActionBar.SetHomeAsUpIndicator(Resource.Drawable.ic_menu);
+            SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+            SupportActionBar.Title = "Hub";
+
+
+            mDrawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            NavigationView navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+
+            navigationView.NavigationItemSelected += (sender, e) =>
+            {
+                e.MenuItem.SetChecked(true);
+                //react to click here and swap fragments or navigate
+                mDrawerLayout.CloseDrawers();
+
+            };
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.toolbarMenu, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Android.Resource.Id.Home:
+                    mDrawerLayout.OpenDrawer(Android.Support.V4.View.GravityCompat.Start);
+                    return true;
+            }
+
+            Toast.MakeText(this, "Zaznaczone: " + item.TitleFormatted,
+                ToastLength.Short).Show();
+            return base.OnOptionsItemSelected(item);
+
+        }
+    }
+}
+
